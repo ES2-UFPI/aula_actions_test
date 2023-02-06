7842b5b3e21aa2d7a18f65d2a70538df4e41026c
Guilherme Avelino <gavelino@gmail.com>
2023-02-06 17:19:21 -0300

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub Actions! Test Actions in pull request. 
+          Aula GitHub Actions! Test Actions in pull request2. 
         </p>
         <a
           className="App-link"
